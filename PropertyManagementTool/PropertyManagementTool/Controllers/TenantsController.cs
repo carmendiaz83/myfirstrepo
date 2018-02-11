27a5dbee6e1e4b2730be95e032bd7002375e43b9
Carmen <carmendiaz83@gmail.com>
2018-02-11 17:21:05 -0500

    First upload

---
+++ PropertyManagementTool/PropertyManagementTool/Controllers/TenantsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PropertyManagementTool.Controllers
+{
+    public class TenantsController : Controller
+    {
+        // GET: Tenants
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
